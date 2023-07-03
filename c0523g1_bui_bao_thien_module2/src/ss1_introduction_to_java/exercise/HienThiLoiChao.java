8b82b40c51c857e6f0c802e31edf8b90ee9c8fe9
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-03 23:13:46 +0700

    em sua bai tap a

--- c0523g1_bui_bao_thien_module2/src/ss1_introduction_to_java/exercise/HienThiLoiChao.java
+++ c0523g1_bui_bao_thien_module2/src/ss1_introduction_to_java/exercise/HienThiLoiChao.java
@@ -1,11 +1,13 @@
 package ss1_introduction_to_java.exercise;
 
+
 import java.util.Scanner;
 
 public class HienThiLoiChao {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        String name = sc.nextLine();
 
         System.out.printf("Hello " + name);
     }
--- c0523g1_bui_bao_thien_module2/src/ss1_introduction_to_java/exercise/UngDungChuyenDoiTienTe.java
+++ c0523g1_bui_bao_thien_module2/src/ss1_introduction_to_java/exercise/UngDungChuyenDoiTienTe.java
@@ -4,6 +4,12 @@ import  java.util.Scanner;
 
 public class UngDungChuyenDoiTienTe {
     public static void main(String[] args) {
-        
+        double vnd = 23000;
+        double usd;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Moi ban nhap so tien USD: ");
+        usd = sc.nextDouble();
+        double quyDoi = usd * 23000;
+        System.out.print("Gia tri VND: " + quyDoi + " VND");
     }
 }
