883683e370e24947558bdbe4d18e1a0d77c468cb
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-05 15:51:21 +0700

    em bo sung bai tap

--- c0523g1_bui_bao_thien_module2/src/ss3_array_and_method/exercise/DemSoLanXuatHien.java
+++ c0523g1_bui_bao_thien_module2/src/ss3_array_and_method/exercise/DemSoLanXuatHien.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class DemSoLanXuatHien {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = "AABBCC";
+        System.out.println("Mời bạn nhập vào chuỗi ký tự");
+        String str = sc.nextLine();
         System.out.println("Mảng ký tự gốc là: " + str);
         System.out.println("Nhập vào một ký tự để kiểm tra sự xuất hiện: ");
-        String input = sc.nextLine().toUpperCase();
+        char input = sc.next().charAt(0);
         System.out.println("Ký tự nhập vào là: " + input);
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (input.equals(str.charAt(i))) {
+            if (input == str.charAt(i)) {
                 count++;
             }
         }
