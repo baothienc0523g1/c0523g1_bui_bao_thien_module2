08baa09d61e69796eacb0f35fd94ac9f040c6d1e
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-06 15:52:44 +0700

    em bo sung bai tap a

---
+++ c0523g1_bui_bao_thien_module2/src/ss4_oop/exercise/Fan.java
@@ -0,0 +1,72 @@
+package ss4_oop.exercise;
+
+public class Fan {
+    static final int low = 1;
+    static final int medium = 2;
+    static final int fast = 3;
+    int speed = low;
+    double radius = 5;
+    String color = "blue";
+    boolean status = false;
+
+    public Fan() {
+    }
+
+    public Fan(int speed, String color, double radius, boolean status) {
+        this.speed = speed;
+        this.color = color;
+        this.radius = radius;
+        this.status = status;
+    }
+
+    public int getSpeed() {
+        return this.speed = speed;
+    }
+
+    public int setSpeed(int speed) {
+        if (speed == 2) {
+            return this.speed = medium;
+        } else if (speed == 1) {
+            return this.speed = low;
+        } else {
+            return speed = fast;
+        }
+    }
+
+    public boolean setStatus(boolean status) {
+        return this.status = status;
+    }
+
+    private String getColor() {
+        return this.color;
+    }
+
+    private double getRadius() {
+        return this.radius;
+    }
+
+    public String toString() {
+        if (this.status == true) {
+            return "This fan is on, speed is " + getSpeed() + ", color is : " + getColor() + ", radius is: " + getRadius();
+        } else {
+            return "This fan is off!";
+        }
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(fast, "yellow", 10, true);
+        Fan fan2 = new Fan(medium, "blue", 5, false);
+        System.out.println("Before");
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+        System.out.println("After");
+        fan1.setSpeed(1);
+        fan1.setStatus(false);
+        System.out.println("");
+        System.out.println(fan1.toString());
+        fan2.setStatus(true);
+        fan2.setSpeed(3);
+        System.out.println("");
+        System.out.println(fan2.toString());
+    }
+}
--- c0523g1_bui_bao_thien_module2/src/ss4_oop/exercise/StopWatch.java
+++ c0523g1_bui_bao_thien_module2/src/ss4_oop/exercise/StopWatch.java
@@ -1,4 +1,39 @@
 package ss4_oop.exercise;
 
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class StopWatch {
+    long startTime;
+    long endTime;
+
+    public StopWatch(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public StopWatch() {
+    }
+
+    public long start() {
+        return this.startTime = System.currentTimeMillis();
+    }
+
+    public long stop() {
+        return this.endTime = System.currentTimeMillis();
+    }
+
+    public String getElapsedTime() {
+        return this.endTime - this.startTime + " ms";
+    }
+
+    public static void main(String[] args) {
+        StopWatch casio = new StopWatch();
+        casio.start();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Test time 1");
+        String str = sc.nextLine();
+        casio.stop();
+        System.out.println(casio.getElapsedTime());
+    }
 }
