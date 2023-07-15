1048445407a2e9fbc415825adf6263769c845976
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-15 13:46:28 +0700

    fix

--- c0523g1_bui_bao_thien_module2/src/ss11_dsa_stack_queue/exercise/Optional2PalindromeString.java
+++ c0523g1_bui_bao_thien_module2/src/ss11_dsa_stack_queue/exercise/Optional2PalindromeString.java
@@ -1,42 +1,42 @@
 package ss11_dsa_stack_queue.exercise;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-public final class Optional2Palindrome extends MyQueueArrayList {
+public final class Optional2PalindromeString {
     public static void checkPalindrome(String string) {
+        Queue<Character> firstHalfString = new LinkedList<>();
+        Stack<Character> secondHalfString = new Stack<>();
         String result = "";
-        ArrayList<Character> firstHalfString = new ArrayList<>();
-        ArrayList<Character> secondHalfString = new ArrayList<>();
-
         String converseString = string.replaceAll(" ","").toLowerCase();
-        int halfLength = converseString.length()/2;
+        int halfLength = converseString.length() / 2;
 
         for (int i = 0; i <= halfLength; i++) {
             firstHalfString.add(converseString.charAt(i));
         }
-        for (int j = converseString.length() - 1; j >= halfLength; j--) {
-            secondHalfString.add(converseString.charAt(j));
+        for (int i = converseString.length() - 1; i >= halfLength; i--) {
+            secondHalfString.push(converseString.charAt(i));
+        }
+        int count = 0;
+        for (int i = 0; i < halfLength; i++) {
+            if (firstHalfString.remove(secondHalfString.get(i)))
+                count++;
         }
 
-        int count = 0;
-        for (int k = 0; k < halfLength; k++) {
-            if (firstHalfString.get(k).equals(secondHalfString.get(k))) {
-                count++;
-            }
-        }
         String correct = "This is a palindrome string";
         String incorrect = "This is NOT a palindrome string";
         result += (count == halfLength) ? correct : incorrect;
         System.out.println(result);
     }
+
     public static void main(String[] args) {
-        String test1 = "Able was I ere I saw Elba";
-        String test2 = "abc bcd def";
-        String test3 = "que euq que euq";
+        String test1 = "abccba  ";
+        String test2 = "Able was I ere I saw Elba  ";
+        String test3 = "abccba aaa  ";
+
         checkPalindrome(test1);
         checkPalindrome(test2);
         checkPalindrome(test3);
     }
-
 }
