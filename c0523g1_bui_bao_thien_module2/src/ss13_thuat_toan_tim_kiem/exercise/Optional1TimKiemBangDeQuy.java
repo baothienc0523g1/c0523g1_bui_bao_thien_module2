8b3f3d4f6a4364999be005ff1c49a1e07adc7a0c
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-17 21:06:38 +0700

    fix

--- c0523g1_bui_bao_thien_module2/src/ss13_thuat_toan_tim_kiem/exercise/Optional1TimKiemBangDeQuy.java
+++ c0523g1_bui_bao_thien_module2/src/ss13_thuat_toan_tim_kiem/exercise/Optional1TimKiemBangDeQuy.java
@@ -1,10 +1,26 @@
 package ss13_thuat_toan_tim_kiem.exercise;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
 
-public class Optional1TimKiemBangDeQuy {
+public class Optional1TimKiemBangDeQuy implements Comparable {
+    public static List<Integer> arr = new ArrayList<>();
+
+    public static List<Integer> summonArr() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("nhập vào số phần tử của mảng: ");
+        int size = scanner.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= size; i++) {
+            System.out.print("Nhập vào phần tử thứ " + i + " : ");
+            int input = scanner.nextInt();
+            arr.add(input);
+            Collections.sort(arr);
+        }
+        return arr;
+    }
+
+
     public static int findValue(List<Integer> arr, int number) {
         int first = 0;
         int last = arr.size() - 1;
@@ -21,32 +37,33 @@ public class Optional1TimKiemBangDeQuy {
         return -1;
     }
 
+    @Override
+    public int compareTo(Object o) {
+        return arr.get(1) - arr.get(2);
+    }
+
     public static void main(String[] args) {
-        int choice = -1;
         Scanner scanner = new Scanner(System.in);
+        int choice = -1;
         do {
-            System.out.println("Bấm số bất kỳ để bắt đầu, 0 để dừng");
+            System.out.println("Bấm phím số bất kỳ để bắt đầu, bấm 0 để dừng");
             choice = scanner.nextInt();
             if (choice != 0) {
-                System.out.print("nhập vào số phần tử của mảng");
-                int size = scanner.nextInt();
-                List<Integer> arr = new ArrayList<>();
-                for (int i = 1; i <= size; i++) {
-                    System.out.println("Nhập vào phần tử thứ: " + i);
-                    int input = scanner.nextInt();
-                    arr.add(input);
-                }
+                List<Integer> arr = summonArr();
+                System.out.println("Mảng sau khi sắp xếp theo thứ tự" + arr);
                 System.out.println("Nhập vào phần tử muốn tìm kiếm");
                 int num = scanner.nextInt();
                 if (findValue(arr, num) != -1) {
                     System.out.println("Tìm thấy phần tử " + num +
                             " tại index " + findValue(arr, num));
                 } else {
-                    System.out.println("không tìm thấy " + num + findValue(arr,num));
+                    System.out.println("không tìm thấy " + num);
                 }
             } else {
                 System.exit(0);
             }
-        } while (true);
+        }
+        while (true);
+
     }
 }
