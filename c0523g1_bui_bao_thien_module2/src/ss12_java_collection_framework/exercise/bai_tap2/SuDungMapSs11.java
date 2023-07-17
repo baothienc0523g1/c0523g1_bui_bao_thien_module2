a0ee15881b5db28ba56235ef5a1bdbe8e49eb050
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-17 11:48:19 +0700

    em nop bai tap a

--- c0523g1_bui_bao_thien_module2/src/ss12_java_collection_framework/exercise/bai_tap2/SuDungMapSs11.java
+++ c0523g1_bui_bao_thien_module2/src/ss12_java_collection_framework/exercise/bai_tap2/SuDungMapSs11.java
@@ -1,2 +1,27 @@
-package ss12_java_collection_framework.exercise.bai_tap1;public class SuDungMapSs11 {
+package ss12_java_collection_framework.exercise.bai_tap2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SuDungMapSs11 {
+    public static void rateCheck(String string) {
+        String[] strings = string.split(" ");
+
+        Map<String, Integer> myMap = new HashMap();
+
+        for (int i = 0; i < strings.length; i++) {
+            String key = strings[i].toLowerCase();
+            int defaultValue = 1;
+            if (!myMap.containsKey(key)) {
+                myMap.put(key, defaultValue);
+            } else {
+                int temp = myMap.get(key) + 1;
+                myMap.remove(key);
+                myMap.put(key, temp);
+            }
+        }
+        for (Map.Entry<String, Integer> map : myMap.entrySet()) {
+            System.out.println("từ " + map.getKey() + " xuất hiện " + map.getValue());
+        }
+    }
 }
---
+++ c0523g1_bui_bao_thien_module2/src/ss12_java_collection_framework/exercise/bai_tap2/TestSuDungMapSs11.java
@@ -0,0 +1,8 @@
+package ss12_java_collection_framework.exercise.bai_tap2;
+
+public class TestSuDungMapSs11 extends SuDungMapSs11{
+    public static void main(String[] args) {
+        String string = "hello World hEllo hEllo bellO japan";
+        rateCheck(string);
+    }
+}
