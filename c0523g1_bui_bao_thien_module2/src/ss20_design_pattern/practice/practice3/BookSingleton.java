0f15e4348dac15fba537712e2a34fa99f2ee3315
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-07-31 21:44:12 +0700

    ss20

---
+++ c0523g1_bui_bao_thien_module2/src/ss20_design_pattern/practice/practice3/BookSingleton.java
@@ -0,0 +1,41 @@
+package ss20_design_pattern.practice.practice3;
+
+public class BookSingleton {
+    private static BookSingleton bookSingleton;
+    private static boolean isLoanedOut;
+    private final String author;
+    private final String title;
+
+    private BookSingleton() {
+        author = "Gamma, Helm, Johnson, and Vlissides";
+        title = "Design Patterns";
+        bookSingleton = null;
+        isLoanedOut = false;
+    }
+    public static BookSingleton borrowBook() {
+        if (!isLoanedOut) {
+            if (book == null) {
+                book = new BookSingleton();
+            }
+            isLoanedOut = true;
+            return book;
+        }
+        return null;
+    }
+
+    public void returnBook(BookSingleton bookReturned) {
+        isLoanedOut = false;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorAndTitle() {
+        return getTitle() + " by " + getAuthor();
+    }
+}
